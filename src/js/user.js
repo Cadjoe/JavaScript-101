7d1a41a0763dc80af00178481c3c6fb3e6fa985b
Joseph Ssebagala <myfuturejos@gmail.com>
2021-12-08 09:45:16 -0500

    Refactored site from GPC to InsightInverse

---
+++ src/js/user.js
@@ -0,0 +1,21 @@
+// 
+// user.js
+// Use this to write your custom JS
+//
+// CUSTOM
+// Can also be included with a regular script tag
+import Typed from "typed.js";
+
+var typed = new Typed('.data-typed', {
+  strings: [
+    'to aid your healing efforts.',
+    'for individuals.',
+    'for children.',
+    'for couples.',
+    'for families.'
+  ],
+  typeSpeed: 40,
+  backSpeed: 40,
+  backDelay: 1000,
+  loop: true,
+});
